fad886b1e9dab0e3a1e74792ba9d4e52a0af80f6
Margi <shahmargi881@gmail.com>
2026-06-19 15:25:05 +0530

    done

---
+++ first_script.py
@@ -0,0 +1,14 @@
+name = input("Enter name:")
+age = int(input("Enter age:"))
+school = input("Enter School:")
+print("Name:",name)
+print("Age:",age)
+print("School:",school)
+
+
+
+print("*")
+print("**")
+print("***")
+print("**")
+print("*") 
